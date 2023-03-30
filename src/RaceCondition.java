f93ae740c0ea8dd95a41e7f65eecb567d8259333
Jin-hyuck <gjwlsgur4866@gmail.com>
2023-03-30 17:54:13 +0900

    [Feat] 재고 관련 쓰레드 단순 활용 (쓰레드 시작 - 종료 - 시작 - 종료)

---
+++ src/RaceCondition.java
@@ -0,0 +1,63 @@
+package src;
+
+public class RaceCondition {
+    public static void main(String[] args) throws InterruptedException {
+        Inventory inventory = new Inventory();
+        IncreaseThread increaseThread = new IncreaseThread(inventory);
+        DecreaseThread decreaseThread = new DecreaseThread(inventory);
+
+        increaseThread.start();
+        increaseThread.join();
+
+        decreaseThread.start();
+        decreaseThread.join();
+
+        System.out.println("items = " + inventory.getItems());
+    }
+
+    public static class DecreaseThread extends Thread{
+        private Inventory inventory;
+
+        public DecreaseThread(Inventory inventory) {
+            this.inventory = inventory;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventory.decrease();
+            }
+        }
+    }
+
+    public static class IncreaseThread extends Thread{
+        private Inventory inventory;
+
+        public IncreaseThread(Inventory inventory) {
+            this.inventory = inventory;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventory.increase();
+            }
+        }
+    }
+
+    public static class Inventory {
+        private int items = 0;
+
+        public void increase() {
+            items++;
+        }
+
+        public void decrease() {
+            items--;
+        }
+
+        public int getItems() {
+            return items;
+        }
+    }
+}
